fa1364821e5355d30c9b71bbd44b23fa11940ce7
Oleg <oleg.bezkorovajniy@gmail.com>
2015-08-16 17:28:02 +0300

    added method POST in UserController and test for it

--- SiaMVC/src/main/java/com/sia/tutorial/web/UserController.java
+++ SiaMVC/src/main/java/com/sia/tutorial/web/UserController.java
@@ -1,19 +1,53 @@
 package com.sia.tutorial.web;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.sia.tutorial.User;
+import com.sia.tutorial.data.UserRepository;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
+	private UserRepository userRepository;
+
+	public UserController() {
+	}
+
+	@Autowired
+	public UserController(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
 	@RequestMapping(value = "/register", method = GET)
 	public String showRegistrationForm() {
 		return "registerForm";
 	}
-	
-	
+
+	@RequestMapping(value = "/register", method = POST)
+	public String processRegistration(@Valid User user, Errors errors) {
+		if (errors.hasErrors()) {
+			return "registerForm";
+		}
+		userRepository.save(user);
+		return "redirect:/user/" + user.getUsername();
+	}
+
+	@RequestMapping(value = "/{username}", method = GET)
+	public String showUserProfile(@PathVariable String username, Model model) {
+		User user = userRepository.findByUsername(username);
+		model.addAttribute(user);
+		return "profile";
+	}
 
 }
--- SiaMVC/src/test/java/com/sia/tutorial/UserControllerTest.java
+++ SiaMVC/src/test/java/com/sia/tutorial/UserControllerTest.java
@@ -1,11 +1,18 @@
 package com.sia.tutorial;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.atLeastOnce;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 
 import org.junit.Test;
 
+import com.sia.tutorial.data.UserRepository;
 import com.sia.tutorial.web.UserController;
 
 import org.springframework.test.web.servlet.MockMvc;
@@ -17,6 +24,28 @@ public class UserControllerTest {
 		UserController controller = new UserController();
 		MockMvc mockMvc = standaloneSetup(controller).build();
 		mockMvc.perform(get("/user/register")).andExpect(view().name("registerForm"));
+	}
+
+	@Test
+	public void shouldProcessRegistration() throws Exception {
+
+		UserRepository mockRepository = mock(UserRepository.class);
+		User unsaved = new User("olegbezk", "24hours", "Oleg", "Bezk", "some@email.com");
+		User saved = new User(24L, "olegbezk", "24hours", "Oleg", "Bezk", "some@email.com");
+		when(mockRepository.save(unsaved)).thenReturn(saved);
+
+		UserController controller = new UserController(mockRepository);
+		MockMvc mockMvc = standaloneSetup(controller).build();
+
+		mockMvc.perform(post("/user/register")
+				.param("firstName", "Oleg")
+				.param("lastName", "Bezk")
+				.param("username", "olegbezk")
+				.param("password", "24hours")
+				.param("email", "some@email.com"))
+				.andExpect(redirectedUrl("/user/olegbezk"));
+
+		verify(mockRepository, atLeastOnce()).save(unsaved);
 	}
 
 }
